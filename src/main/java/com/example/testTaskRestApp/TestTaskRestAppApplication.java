30e6d48a91f17a00fcb0007fab95d7e1a188dbaf
Юрий <yurikua11@gmail.com>
2019-01-25 17:04:45 +0200

    Initial commit

---
+++ src/main/java/com/example/testTaskRestApp/TestTaskRestAppApplication.java
@@ -0,0 +1,14 @@
+package com.example.testTaskRestApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTaskRestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestTaskRestAppApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/example/testTaskRestApp/TestTaskRestAppApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.testTaskRestApp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestTaskRestAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
